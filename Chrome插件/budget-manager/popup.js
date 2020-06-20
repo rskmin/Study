77dc8635e9c66f2d82ad9a6cff304e0e8eea881e
rskmin <850667106@qq.com>
2020-06-20 11:26:02 +0800

    feat: chrome/预算管理缓存

--- Chrome插件/budget-manager/popup.js
+++ Chrome插件/budget-manager/popup.js
@@ -1,13 +1,23 @@
 /* eslint-disable no-undef */
 $(function() {
   const Amount = $('#amount')
+  const Total = $('#total')
+  let total = 0
+  // 获取缓存 & 初始化总支出
+  chrome.storage.sync.get('total', function(budget) {
+    total = Number(budget.total)
+    Total.text(total)
+  })
+  // 计算总支出
   $('#spendAmount').click(function() {
-    let newTotal = 0
     let amount = Number(Amount.val())
     if (amount) {
-      newTotal += amount
+      total += amount
+      chrome.storage.sync.set({
+        'total': total
+      })
+      Total.text(total)
+      Amount.val('')
     }
-    $('#total').text(newTotal)
-    Amount.val('')
   })
 })
