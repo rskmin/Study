86101a00ab3b697a1eefa2ec7e3fd43dd867d38e
LiJiaxin <wb-ljx797360@alibaba-inc.com>
2020-08-26 13:54:03 +0800

    js&html-css images

--- React/ts-version/0.react/src/index.tsx
+++ React/ts-version/0.react/src/index.tsx
@@ -1,38 +1,57 @@
 /* eslint-disable no-invalid-this */
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-interface Props {
-
+interface IProps {
+  render: (value: IState) => React.ReactNode
 }
 
-interface State {
+interface IState {
   x: number
   y: number
 }
 
-class MouseTracker extends Component<Props, State> {
+class MouseTracker extends Component<IProps, IState> {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       x: 0,
       y: 0
-    }
+    };
   }
   handleMouseMove = (event: React.MouseEvent) => {
     this.setState({
       x: event.clientX,
       y: event.clientY
-    })
+    });
   }
   render() {
     return (
-      <div onMouseMove={this.handleMouseMove}>
-        <p>请现在移动鼠标</p>
-        <p>当前鼠标的位置</p>
+      <div onMouseMove={this.handleMouseMove} style={{ border: '1px solid red' }}>
+        {this.props.render(this.state)}
       </div>
+    );
+  }
+}
+
+// 普通函数组件
+const MyComponent = (value: IState) => (
+  <div>
+    <p>请现在移动鼠标</p>
+    <p>当前鼠标的位置x: {value.x},y: {value.y}</p>
+  </div>
+)
+
+function withMouseTracker(OldComponent: React.FC<IState>) {
+  return (props: any) => {
+    return (
+      <MouseTracker render={
+        mouseProps => <OldComponent {...props} {...mouseProps} />
+      } />
     )
   }
 }
 
-ReactDOM.render(<MouseTracker />, document.getElementById('root'))
+const WithMouseTrackerMyComponent = withMouseTracker(MyComponent);
+
+ReactDOM.render(<WithMouseTrackerMyComponent />, document.getElementById('root'));
