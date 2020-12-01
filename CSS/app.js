93192cdf47eb9f59f068b2483c89adce4ac4f188
rskmin <850667106@qq.com>
2020-12-01 23:25:37 +0800

    CSS

--- CSS/app.js
+++ CSS/app.js
@@ -1,48 +1,85 @@
 const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
+const fsp = fs.promises;
 const express = require('express');
 const resolve = (...arr) => path.resolve(__dirname, ...arr)
 
-// 读取ejs模板文件生成模板
-const str = fs.readFileSync(resolve('public/index.ejs'), {
-  encoding: 'utf-8'
-});
-let template = ejs.compile(str);
+/**
+ * 读取 ejs 生成模版
+ * @returns {ejs.TemplateFunction}
+ */
+async function readEjs() {
+  const str = await fsp.readFile(resolve('public/index.ejs'), {
+    encoding: 'utf-8'
+  });
+  const template = ejs.compile(str);
+  return template;
+}
 
-// 读取文件夹数据
-let dirList = fs.readdirSync(__dirname);
-dirList = dirList.reduce((arr, name) => {
-  const excludeDir = ['public', 'node_modules'];
-  const isDirectory = fs.statSync(resolve(name)).isDirectory();
-  if (isDirectory && !excludeDir.includes(name)) {
-    let contentList = fs.readdirSync(resolve(name));
-    // 去除非文件夹内容
-    contentList = contentList.reduce((arr, item) => {
-      const isDir = fs.statSync(resolve(name, item)).isDirectory();
-      if (isDir) {
-        arr.push(item);
-      }
-      return arr;
-    }, []);
-    arr.push({
-      name,
-      contentList,
-    });
-  }
-  return arr;
-}, []);
+/**
+ * @typedef {Object} DirItem
+ * @property {string[]} contentList
+ * @property {string} dirname
+ */
+/**
+ * 读取文件夹数据
+ * @returns {DirItem[]}
+ */
+async function readDirList() {
+  let dirList = await fsp.readdir(__dirname);
+  dirList = dirList.reduce((arr, dirname) => {
+    const excludeDir = ['public', 'node_modules'];
+    const isDirectory = fs.statSync(resolve(dirname)).isDirectory();
+    if (isDirectory && !excludeDir.includes(dirname)) {
+      let contentList = fs.readdirSync(resolve(dirname));
+      // 去除非文件夹内容
+      contentList = contentList.reduce((arr, item) => {
+        const isDir = fs.statSync(resolve(dirname, item)).isDirectory();
+        if (isDir) {
+          arr.push(item);
+        }
+        return arr;
+      }, []);
+      arr.push({
+        dirname,
+        contentList,
+      });
+    }
+    return arr;
+  }, []);
+  return dirList;
+}
+/**
+ * 读取基本样式
+ * @returns {string}
+ */
+async function readBasicCss() {
+  const basicCss = await fsp.readFile(resolve('public/basic.css'));
+  const styleTag = `<style>${basicCss.toString()}</style>`
+  return styleTag;
+}
 
-// 获取数据渲染页面
-const data = {
-  PUBLIC_URL: '/public',
-  dirList,
-};
-const html = template(data);
+async function runServer() {
+  const template = await readEjs();
+  const dirList = await readDirList();
+  const basicCss = await readBasicCss();
 
-// 静态服务
-const app = express();
-const port = 8080;
-app.use(express.static(__dirname));
-app.get('/', (req, res) => res.end(html));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  // 获取数据渲染页面
+  const data = {
+    PUBLIC_URL: '/public',
+    dirList,
+    basicCss,
+  };
+  const html = template(data);
+
+  // 静态服务
+  const app = express();
+  const port = 8080;
+  app.use(express.static(__dirname));
+  app.get('/', (req, res) => res.end(html));
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
+runServer();
+
+
--- CSS/public/index.js
+++ CSS/public/index.js
@@ -1,8 +1,25 @@
 const iframe = document.querySelector('#content');
 const menuList = document.querySelector('#app-menu-list');
+
+/**
+ * @typedef {Object} DirItem
+ * @property {string[]} contentList
+ * @property {string} dirname
+ */
+
+/** @var 目录列表 @type {DirItem[]} */
+const dirList = window.__dirList__;
+
+window.addEventListener('load', () => {
+  const src = location.hash.slice(1) || `/${dirList[0].dirname}/${dirList[0].contentList[0]}`;
+  iframe.src = src;
+})
+
 menuList.addEventListener('click', (e) => {
   let { dir, dirname } = e.target.dataset;
   if (dir && dirname) {
-    iframe.src = `/${dir}/${dirname}`;
+    const src = `/${dir}/${dirname}`
+    iframe.src = src;
+    location.hash = src;
   }
 })
